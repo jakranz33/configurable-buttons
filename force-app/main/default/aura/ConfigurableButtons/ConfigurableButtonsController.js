5fd9b7c383f265b7ead4541e64fb0c7c6c61c199
Jeffrey Kranz <jakranz33@gmail.com>
2018-11-02 01:52:33 -0500

    changes

--- force-app/main/default/aura/ConfigurableButtons/ConfigurableButtonsController.js
+++ force-app/main/default/aura/ConfigurableButtons/ConfigurableButtonsController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit : function(component, event, helper) {
+        var action = component.get("c.getButtonConfigLabel");
+        action.setParams({
+            configName : 'My Button'
+        });
+        
+        component.get("v.button01_mdt");
     }
 })
